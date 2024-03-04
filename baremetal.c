cc833fffc7f31521bf0de859b4b89a5c4fce6b8d
amellalalex <github@alexamellal.com>
2024-03-04 14:37:15 +0100

    Drained the swamp.

--- baremetal.c
+++ baremetal.c
@@ -57,18 +57,3 @@ int main(void) {
         }
         return 0;
 }
-
-__attribute__((naked, noreturn)) void _reset(void) {
-        extern long _sbss, _ebss, _sdata, _edata, _sidata;
-        for(long *dst = &_sbss; dst < &_ebss; dst++) *dst = 0;
-        for(long *dst = &_sdata, *src = &_sidata; dst < &_edata;) *dst++ = *src++;
-
-        main();
-        for(;;) (void) 0;
-}
-
-extern void _estack(void);
-
-__attribute__((section(".vectors"))) void (*const tab[16 + 91])(void) = {
-        _estack, _reset
-};
